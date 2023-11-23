352c36cb0c4ed2b91db1df2985d8b5bf50a1528d
Imran Parthib <imranparthib13@gmail.com>
2023-11-24 03:27:55 +0600

    Bee1020 solved

---
+++ Beecrowd Problems/1020.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pAge;
+    // cin >> pAge;
+    pAge = 800;
+    int pYear = pAge/365;
+    int remindAge1 = pAge % 365;
+    int pMonth = remindAge1 /30;
+    int pDays= remindAge1 % 30;
+
+    cout<<pYear<<endl;
+    cout<<pMonth<<endl;
+    cout<<pDays<<endl;
+    
+    return 0;
+}
