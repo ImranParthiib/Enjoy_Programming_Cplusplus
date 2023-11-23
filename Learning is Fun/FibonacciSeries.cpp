49e937f5cadaefa30281222ff702b34fadf0c4bc
Imran Parthib <imranparthib13@gmail.com>
2023-11-24 01:36:45 +0600

    Practicing FibonacciAgain

---
+++ Learning is Fun/FibonacciSeries.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int firstTerm,secondTerm,lastTerm,totalTerm;
+    cin>>totalTerm;
+    firstTerm = 0;
+    secondTerm = 1;
+
+    if(totalTerm==0){
+        return 0;
+    }
+    cout<<firstTerm<<','<<secondTerm;
+
+    lastTerm = firstTerm + secondTerm;
+
+    for(int i =2;i<totalTerm;i++){
+        cout<<','<<lastTerm;
+        firstTerm = secondTerm;
+        secondTerm = lastTerm;
+        lastTerm = firstTerm + secondTerm;
+    }
+    return 0;
+
+}
