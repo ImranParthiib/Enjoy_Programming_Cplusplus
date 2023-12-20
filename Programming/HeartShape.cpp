5c089fcd371ba86bf251df28038b7d178fad386c
Imran Parthib <imranparthib13@gmail.com>
2023-12-21 03:04:58 +0600

    HeartSHape added to Programming

---
+++ Programming/HeartShape.cpp
@@ -0,0 +1,244 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch = 3;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 8 || i == 9 || i == 7 || i == 6 || i == 25 || i == 26 || i == 27 || i == 28)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 4 || i == 34 || i == 13 || i == 26)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 3 || i == 35 || i == 15 || i == 24)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 2 || i == 31 || i == 14 || i == 19)
+        {
+            cout << ch;
+        }
+        if (i == 7)
+        {
+            cout << "Y";
+        }
+        if (i == 8)
+        {
+            cout << "O";
+        }
+        if (i == 9)
+        {
+            cout << "U";
+        }
+        if (i == 24)
+        {
+            cout << "A";
+        }
+        if (i == 25)
+        {
+            cout << "R";
+        }
+        if (i == 26)
+        {
+            cout << "E";
+        }
+        else
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 1 || i == 37 || i == 20)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 1 || i == 38)
+        {
+
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 1 || i == 37)
+        {
+            cout << ch;
+        }
+        if (i == 20)
+        {
+            cout << "S";
+        }
+        if (i == 21)
+        {
+            cout << "O";
+        }
+        else
+        {
+
+            cout << " ";
+        }
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 2 || i == 37)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 4 || i == 26)
+        {
+            cout << ch;
+        }
+        if (i == 8)
+        {
+            cout << "C";
+        }
+        if (i == 9)
+        {
+            cout << "U";
+        }
+        if (i == 10)
+        {
+            cout << "T";
+        }
+        if (i == 11)
+        {
+            cout << "E";
+        }
+        if (i == 16)
+        {
+            cout << "&";
+        }
+        if (i == 19)
+        {
+            cout << "L";
+        }
+        if (i == 20)
+        {
+            cout << "O";
+        }
+        if (i == 21)
+        {
+            cout << "V";
+        }
+        if (i == 22)
+        {
+            cout << "E";
+        }
+        if (i == 23)
+        {
+            cout << "L";
+        }
+        if (i == 24)
+        {
+            cout << "Y";
+        }
+        else
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 6 || i == 34)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 8 || i == 32)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 10 || i == 30)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 13 || i == 27)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 15 || i == 25)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 18 || i == 20)
+        {
+            cout << ch;
+        }
+        if (i == 19)
+        {
+            cout << "!!!";
+        }
+        cout << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= 40; i++)
+    {
+        if (i == 21)
+        {
+            cout << ch;
+        }
+        cout << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
