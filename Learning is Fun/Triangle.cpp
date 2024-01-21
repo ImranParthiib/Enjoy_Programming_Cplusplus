3c66dc2913a1c54423dc87632a30990d596771fa
Imran Parthib <imranparthib13@gmail.com>
2024-01-21 23:38:44 +0600

    Workinng with Array and iteration to get pattern

---
+++ Learning is Fun/Triangle.cpp
@@ -0,0 +1,26 @@
+// C++ program to print
+// equilateral triangle
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+    int n;
+    cin >> n;
+
+    // ith row has n-i leading spaces
+    // and i elements
+    for (int i = 1; i <= n; i++)
+    {
+        // n-i leading spaces
+        for (int j = 0; j < n - i; j++)
+            cout << " *";
+
+        // i elements
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Programming/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is Imran Parthib" << endl;
+    string A[3] = {"Imran", "Parthib"};
+
+    for (int i = 0; i < A->length; i++)
+    {
+        cout << A[i];
+    }
+
+    return 0;
+}
