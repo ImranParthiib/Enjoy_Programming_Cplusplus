6be8913f878678dbb3a3400db985d0c5af824a58
Imran Parthib <imranparthib13@gmail.com>
2024-02-02 22:35:21 +0600

    setup completed

--- Codechef Problem/Codechef Learning/intro.cpp
+++ Codechef Problem/Codechef Learning/intro.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
+    /**
+    * MyMethod 
+    * ! cout stands for => character output stream to output data to screen
+    * ? <<: This is the stream insertion operator.
+    * ? It is used with cout to send data to the output stream.
+    * * Semicolon is used to end a statement, like period (.) in English.
+    * ! << endl : to have the next outputs be on the next line
+    * @param 
+    * TODO:
+    */
     cout<<12;
 }
