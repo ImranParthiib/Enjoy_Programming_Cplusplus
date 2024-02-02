f3865775d0f3b923fe3dc0f9eabfcb49d038e3b1
Imran Parthib <imranparthib13@gmail.com>
2024-02-02 23:04:42 +0600

    Syntax breakdown

---
+++ Codechef Problem/Codechef Learning/Syntax.cpp
@@ -0,0 +1,6 @@
+#include<iostream>//! contents of the iostream library.This library is necessary for input and output operations in C++. Think of it like giving your program the ability to speak and listen
+using namespace std;//* C++ has something called 'namespaces' which are used to organize code into logical groups. By stating using namespace std;, you're telling the program that you'll be using functions from std (standard) namespace.
+
+int main(){ // ! int main(){} === This is the main function. Every C++ program must have a main function. This is the starting point of your program. When you run the program, the code inside the main function is what gets executed
+    cout << "Syntax Breakdown"<<endl;// * cout stands for => character output stream to output data to screen
+}
