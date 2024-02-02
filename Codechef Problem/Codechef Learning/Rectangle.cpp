861ab45429bdf83a7d49cd3bc939b2da6241687e
Imran Parthib <imranparthib13@gmail.com>
2024-02-02 22:38:55 +0600

    Area & Perimeter of Rectangle

---
+++ Codechef Problem/Codechef Learning/Rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /**
+    * ! cout stands for => character output stream to output data to screen
+    */
+      // Add the formula for area of 11 and 13
+    cout << 11 * 13 << endl;
+    
+    // Add the formula for perimeter of 11 and 13
+    cout << 2 * ( 11 + 13 );
+}
