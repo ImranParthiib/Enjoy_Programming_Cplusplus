dab996a309373baedf115d74ef0462c67cc75f3e
ImranParthib <imranparthib13@gmail.com>
2024-03-10 14:40:38 +0600

    lowestValueArray

---
+++ Programming/lowestValue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int myScore[] = { 23,8,90,7,67,89 ,3};
+    int n = sizeof(myScore) / sizeof(int);
+    cout << n << endl;
+    int lowestValue = myScore[0];
+
+    for (int i = 0; i < n; i++) {
+        if (myScore[i] < lowestValue) {
+            lowestValue = myScore[i];
+        }
+    }
+    cout << lowestValue << endl;
+    return 0;
+}
