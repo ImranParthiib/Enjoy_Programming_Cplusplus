fe9fe86dcd0cd57be75f81b527f945fa431538f9
ImranParthib <imranparthib13@gmail.com>
2024-03-16 23:36:04 +0600

    🎨 Starting Todays Journey

--- Learning is Fun/FibonacciSeries.cpp
+++ Learning is Fun/FibonacciSeries.cpp
@@ -1,22 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
 
-    int firstTerm,secondTerm,lastTerm,totalTerm;
-    cin>>totalTerm;
+
+int main() {
+
+    long long firstTerm, secondTerm, lastTerm, totalTerm;
+    cin >> totalTerm;
     firstTerm = 0;
     secondTerm = 1;
 
-    if(totalTerm==0){
-        return 0;
-    }
-    cout<<firstTerm<<','<<secondTerm;
+        if (totalTerm == 0) {
+            return 0;
+        }
+    cout << firstTerm << ',' << secondTerm;
 
     lastTerm = firstTerm + secondTerm;
 
-    for(int i =2;i<totalTerm;i++){
-        cout<<','<<lastTerm;
+    for (int i = 2;i < totalTerm;i++) {
+        cout << ',' << lastTerm;
         firstTerm = secondTerm;
         secondTerm = lastTerm;
         lastTerm = firstTerm + secondTerm;
