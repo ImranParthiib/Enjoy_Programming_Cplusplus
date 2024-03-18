a5a89e0d556bbfa9ba73097a64c8f1cb892858be
ImranParthib <imranparthib13@gmail.com>
2024-03-18 23:59:11 +0600

    ✅ Bit++ easycode

--- CodeForcesContest/Bit++.cpp
+++ CodeForcesContest/Bit++.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-     
+    int t;
+    cin >> t;
+    int x = 0;
+    while (t--) {
+        string str;
+        cin >> str;
+        int n = str.length();
+        if (str[n - 1] == '+' || str[0] == '+') {
+            ++x;
+        }
+        else {
+            --x;
+        }
+    }
+    cout << x << endl;
     return 0;
 }
