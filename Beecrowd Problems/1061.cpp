decaef4eec64400e51471cdbf701daf190321dff
ImranParthib <imranparthib13@gmail.com>
2024-10-06 02:21:28 +0600

    ✨ feat: Add code for Beecrowd Problems 2456, 1037, 1061, and 1048

--- Beecrowd Problems/1061.cpp
+++ Beecrowd Problems/1061.cpp
@@ -3,6 +3,38 @@ using namespace std;
 
 int main() {
 
+    int d1, h1, m1, s1, d2, h2, m2, s2;
+    string day;
     
+    // Read the first date and time
+    cin >> day >> d1;
+    cin >> h1 >> day >> m1 >> day >> s1;
+    
+    // Read the second date and time
+    cin >> day >> d2;
+    cin >> h2 >> day >> m2 >> day >> s2;
+
+    int d = d2 - d1;
+    int h = h2 - h1;
+    int m = m2 - m1;
+    int s = s2 - s1;
+
+    if (s < 0) {
+        s += 60;
+        m--;
+    }
+    if (m < 0) {
+        m += 60;
+        h--;
+    }
+    if (h < 0) {
+        h += 24;
+        d--;
+    }
+
+    cout << d << " dia(s)" << endl;
+    cout << h << " hora(s)" << endl;
+    cout << m << " minuto(s)" << endl;
+    cout << s << " segundo(s)" << endl;
     return 0;
 }
--- Beecrowd Problems/2138.cpp
+++ Beecrowd Problems/2138.cpp
@@ -1,32 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    string str;
-    str = "15000228888888";
-    int n = str.size();
+int main() {
+    string n;
 
-    string temp3;
-    for (int i = 0; i < n; i++)
-    {
-        int temp1 = 0;
-        for (int j = i + 1; j < n; j++)
-        {
-            int temp2 = 0;
-            if (str[i] == str[j])
-            {
-                temp1++;
+    while (cin >> n) {
+        vector<int> freq(10, 0);
 
-                if (temp1 > temp2)
-                {
-                    int temp2 = temp1;
-                    temp3 = str[i];
-                }
+        // Count the frequency of each digit
+        for (char digit : n) {
+            freq[digit - '0']++;
+        }
+
+        // Find the most frequent digit
+        int max_freq = 0;
+        int most_frequent_digit = 0;
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > max_freq || (freq[i] == max_freq && i > most_frequent_digit)) {
+                max_freq = freq[i];
+                most_frequent_digit = i;
             }
         }
+
+        // Output the most frequent digit
+        cout << most_frequent_digit << endl;
     }
-    cout << temp3 << endl;
 
     return 0;
 }
