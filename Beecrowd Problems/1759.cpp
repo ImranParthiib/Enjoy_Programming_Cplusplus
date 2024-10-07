a2c2230788450a6056bd28bd86e71e7e9abd0945
ImranParthib <imranparthib13@gmail.com>
2024-10-07 23:47:18 +0600

    🚀 feat: Add code for Beecrowd Problems 1167 and 1759

---
+++ Beecrowd Problems/1759.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Ho";
+        if (i < n - 1) cout << " ";
+    }
+    cout << "!" << endl;
+    return 0;
+}
